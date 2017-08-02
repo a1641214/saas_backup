45137c6699b09f06150d791151f7dea369abe3ee
Markus James Andersons <markus.andersons@student.adelaide.edu.au>
2017-08-02 11:52:37 +0930

    #47 Seeds created and length added to sessions

--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Session, type: :model do
     subject do
-        described_class.new(time: Time.new(2017, 7, 30, 10, 0, 0, '+09:30'),
+        described_class.new(time: Time.new(2017, 7, 30, 10, 0, 0, '+09:30'), length: 1,
                             day: 'Monday', weeks: [1, 3, 5, 7, 9, 11], component_code: 'LE01')
     end
     it 'is valid with valid attributes' do
@@ -22,6 +22,10 @@ RSpec.describe Session, type: :model do
     end
     it 'is not valid without a component code' do
         subject.component_code = nil
+        expect(subject).to_not be_valid
+    end
+    it 'is not valid without a length' do
+        subject.length = nil
         expect(subject).to_not be_valid
     end
     it 'should have one component' do
