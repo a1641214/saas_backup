a7e7aff5f1a3f8dcdc1b7c0aa7d088f27242332a
Jacobious52 <jacobgonzalez5252@hotmail.com>
2017-08-06 15:47:56 +0930

    + Added compoment importing and reading * changed courses list to a dict for fast lookup + added directory import for finding csv files

--- script/import.py
+++ script/import.py
@@ -15,6 +15,7 @@ class Course:
         self.id = id
         self.name = name
         self.catalog_number = catalog_number
+        self.components = []
 
     # print as a csv row
     def __repr__(self):
@@ -24,16 +25,28 @@ class Course:
     def __eq__(self, other):
         return self.id == other.id
 
+
+class Component:
+    def __init__(self, id, type):
+        self.id = id
+        self.type = type
+
+    def __repr__(self):
+        return '"%s"' % self.type
+
+    def __eq__(self, other):
+        return self.type == other.type
+
 # import courses from the course catalog
 def importCourses(filename):
-    courses = []
+    courses = {}
     with open(filename) as f:
         reader = csv.reader(f)
         for row in reader:
             id = row[1]
             name = row[4]
             course = Course(id, name, None)
-            courses.append(course)
+            courses[id] = course
     return courses
 
 # fill offering cataglog numbers from the offerings csv
@@ -42,16 +55,36 @@ def fillCourseOfferings(filename, courses):
         reader = csv.reader(f)
         for row in reader:
             id = row[1]
-            for course in courses:
-                if course.id == id:
-                    # COMPSCI 3005
-                    course.catalog_number = '%s %s' % (row[4], row[5])
-    return courses
+            if id in courses:
+                # COMPSCI 3005
+                courses[id].catalog_number = '%s %s' % (row[4], row[5])
+
+# read in all the components and link them to their courses
+def importComponentsAndLink(filename, courses):
+    components = []
+    with open(filename) as f:
+        reader = csv.reader(f)
+        for row in reader:
+            id = row[1]
+            type = row[-1]
+            component = Component(id, type)
+            # link component and course
+            if id in courses:
+                course = courses[id]
+                if component not in course.components:
+                    courses[id].components.append(component)
+                    components.append(component)
+
+    return components
+
+directory = raw_input('directory? ')
 
 # TODO: find a bettr place for the course csv files
-courses = importCourses('../courses/CM_CRSE_CAT_ECMS-6383074.csv')
-courses = fillCourseOfferings('../courses/CM_CRSE_CAT_ECMS_OFFERINGS-6383075.csv', courses)
+courses = importCourses('%s/CM_CRSE_CAT_ECMS-6383074.csv' % directory)
+fillCourseOfferings('%s/CM_CRSE_CAT_ECMS_OFFERINGS-6383075.csv' % directory, courses)
+components = importComponentsAndLink('%s/CM_CRSE_CAT_ECMS_COMPONENTS-6383069.csv' % directory, courses)
 
 # print them for now
-for course in courses:
+for course in courses.itervalues():
     print(course)
+    print('\t%s' % course.components)
