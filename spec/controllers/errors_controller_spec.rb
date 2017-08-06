1037fa947a3367ee4540410f530932fa0e1c090f
Jake Lane <me@jakelane.me>
2017-08-06 14:37:14 +0930

    #50 Fix unit test for error pages

--- spec/controllers/errors_controller_spec.rb
+++ spec/controllers/errors_controller_spec.rb
@@ -1,17 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe ErrorsController, type: :controller do
-    before do
-        method = Rails.application.method(:env_config)
-        expect(Rails.application).to receive(:env_config).with(no_args) do
-            method.call.merge(
-                'action_dispatch.show_exceptions' => true,
-                'action_dispatch.show_detailed_exceptions' => false,
-                'consider_all_requests_local' => false
-            )
-        end
-    end
-
     describe 'Internal Server Error' do
         it 'returns http internal server error' do
             get :internal_server_error
@@ -21,7 +10,7 @@ RSpec.describe ErrorsController, type: :controller do
 
     describe 'Unprocessable Entity' do
         it 'returns http unprocessable entity' do
-            get :change_rejected
+            get :unprocessable_entity
             expect(response).to have_http_status(:unprocessable_entity)
         end
     end
