3d154b15bd783c5ce59d3f7bd518e6cb3939e278
janicehm10 <janicehm10@gmail.com>
2017-08-14 10:20:54 +0000

    removed students from demo#index

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,10 +3,8 @@ class DemoController < ApplicationController
     def index
         @path = "db/csv/"
         @courses = ImportFile.importCourses(@path + "CM_CRSE_CAT_ECMS-6383074.csv")
-        @students = ImportFile.importStudents(@path + 'EN_BY_CLASS_ECMS-6384857.csv')
         ImportFile.fillCourseOfferings(@path + 'CM_CRSE_CAT_ECMS_OFFERINGS-6383075.csv',@courses)
         ImportFile.importComponentsAndLink(@path + 'CM_CRSE_CAT_ECMS_COMPONENTS-6383069.csv',@courses)
-        ImportFile.importStudentInfo(@path + 'EN_BY_CLASS_ECMS-6384857.csv', @students)
     end
     
     def display_student 
@@ -18,5 +16,10 @@ class DemoController < ApplicationController
         ImportFile.importComponentsAndLink(@path + 'CM_CRSE_CAT_ECMS_COMPONENTS-6383069.csv',@courses)
         ImportFile.fillStudentsWithCourses(@students,@classes,@courses)
     end    
-        
+    
+    def student_object
+        @students.each do |student|
+            Student.create(student.id) 
+        end    
+    end    
 end
