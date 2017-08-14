030dfeca22ebde1851f142aabcf4c843da903bb9
Markus James Andersons <markus.andersons@student.adelaide.edu.au>
2017-08-14 18:13:53 +0930

    #59 Array of all sessions created

--- lib/ImportFile.rb
+++ lib/ImportFile.rb
@@ -77,14 +77,29 @@ module ImportFile
     end
 
     class Session
-        def initialize(time, day, weeks, length, component_code)
+        def initialize(time, day, weeks, length, component_code, course_id)
             @time = time
             @day = day
             @weeks = weeks
             @length = length
             @component_code = component_code
+            @course_id = course_id
         end
+
+        # get values
+        attr_reader :time
+
+        attr_reader :day
+
+        attr_reader :weeks
+
+        attr_reader :length
+
+        attr_reader :component_code
+
+        attr_reader :course_id
     end
+
 
     # import courses from the course catalog
     def self.importCourses(filename)
@@ -137,31 +152,42 @@ module ImportFile
         return components
     end
 
+    CONST_DAYS_ARRAY = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday].freeze
     # Import the session activity data
     def self.importSessions(filename)
-        sessions = nil
-        CSV.foreach(filename, :headers => true, :encoding => 'iso-8859-1:utf-8') do |row|
-            weeks_bin = row[13]
+        sessions = []
+        CSV.foreach(filename, headers: true, encoding: 'iso-8859-1:utf-8') do |row|
+            weeks_bin = row[13].to_i
             scheduled_time = row[16]
-            if (scheduled_time == "" || scheduled_time == nil)
-                break
-            end
+            break if scheduled_time == '' || scheduled_time.nil?
             days_bin = scheduled_time.scan(/[$]\d+[$]/)[1][1..-2].to_i
             time_bin = scheduled_time.scan(/[$]\d+[$]/)[2][1..-2].to_i
-            days_array = (0...days_bin.bit_length).map { |n| days_bin[n] }.reverse
-            time_array = (0...time_bin.bit_length).map { |n| time_bin[n] }.reverse
+            days_array = (0...days_bin.bit_length).map { |n| days_bin[n] }
+            time_array = (0...time_bin.bit_length).map { |n| time_bin[n] }
             weeks_array = (0...weeks_bin.bit_length).map { |n| weeks_bin[n] }.reverse
             course_id = row[6][2, 6]
-            #course = Course.find(course_id)
-            length = row[12]    # duration of class in minutes
+            length = row[12] # duration of class in minutes
             component_code = row[1][-8..-1]
             component_code = component_code[0, 2] + component_code[-2, 2]
-            #weeks = ????
-            session = Session.new(time, day, weeks_array, length, component_code)
+
+            # determine time
+            start_hours = -1
+            start_minutes = -30
+            time_array.each_with_index do |val, index|
+                start_hours += 1 if index.even?
+                start_minutes = (start_minutes + 30) % 60
+                break if val == 1
+            end
+            time = Time.new(2017, 1, 1, start_hours, start_minutes, 0, '+09:30')
+
+            # determine day
+            days_array.each_with_index do |val, index|
+                if val == 1
+                    sessions.push(Session.new(time, CONST_DAYS_ARRAY[index], weeks_array, length, component_code, course_id))
+                end
+            end
         end
+        sessions
     end
 
-    def self.mapSessionsToComponents(session_array)
-        # TODO
-    end
 end
