b28f7e2546b8c54acbd190289c5f5a2df93e3db7
Jake Lane <jake.lane@student.adelaide.edu.au>
2017-08-18 15:36:06 +0930

    Revert "Feature/fix rubocop issues for import file"

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,10 +1,10 @@
-require 'import_file'
+require 'ImportFile'
 class DemoController < ApplicationController
     def index
         @path = 'db/csv/'
-        @courses = ImportFile.import_courses(@path + 'CM_CRSE_CAT_ECMS-6383074.csv')
-        ImportFile.fill_course_offerings(@path + 'CM_CRSE_CAT_ECMS_OFFERINGS-6383075.csv', @courses)
-        ImportFile.import_components_and_link(@path + 'CM_CRSE_CAT_ECMS_COMPONENTS-6383069.csv', @courses)
+        @courses = ImportFile.importCourses(@path + 'CM_CRSE_CAT_ECMS-6383074.csv')
+        ImportFile.fillCourseOfferings(@path + 'CM_CRSE_CAT_ECMS_OFFERINGS-6383075.csv', @courses)
+        ImportFile.importComponentsAndLink(@path + 'CM_CRSE_CAT_ECMS_COMPONENTS-6383069.csv', @courses)
 
         return unless Course.count.zero?
 
@@ -45,12 +45,12 @@ class DemoController < ApplicationController
 
     def display_student
         @path = 'db/csv/'
-        @courses = ImportFile.import_courses(@path + 'CM_CRSE_CAT_ECMS-6383074.csv')
-        @students = ImportFile.import_students(@path + 'EN_BY_CLASS_ECMS-6384857.csv')
-        @classes = ImportFile.import_classes(@path + 'CLS_CMBND_SECT_FULL-6385825.csv')
-        ImportFile.fill_course_offerings(@path + 'CM_CRSE_CAT_ECMS_OFFERINGS-6383075.csv', @courses)
-        ImportFile.import_components_and_link(@path + 'CM_CRSE_CAT_ECMS_COMPONENTS-6383069.csv', @courses)
-        ImportFile.fill_students_with_courses(@students, @classes, @courses)
+        @courses = ImportFile.importCourses(@path + 'CM_CRSE_CAT_ECMS-6383074.csv')
+        @students = ImportFile.importStudents(@path + 'EN_BY_CLASS_ECMS-6384857.csv')
+        @classes = ImportFile.importClasses(@path + 'CLS_CMBND_SECT_FULL-6385825.csv')
+        ImportFile.fillCourseOfferings(@path + 'CM_CRSE_CAT_ECMS_OFFERINGS-6383075.csv', @courses)
+        ImportFile.importComponentsAndLink(@path + 'CM_CRSE_CAT_ECMS_COMPONENTS-6383069.csv', @courses)
+        ImportFile.fillStudentsWithCourses(@students, @classes, @courses)
     end
 
     def student_object
