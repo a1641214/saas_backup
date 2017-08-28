eba56cdef99c30edb2ad45017d913b86e32a6fac
a1641214 <a1641214@gmail.com>
2017-08-28 02:13:01 +0000

    fix pull request issue#104: revert change on application.html.haml

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
     resources :clash_requests
     resources :clash_resolution
     root to: 'clash_requests#index'
+
     get 'send_email', to: 'clash_requests#send_email', as: :send_email
     get 'confirmation' => 'clash_requests#confirmation', :as => :confirmation
     get 'demo' => 'demo#index', :as => :demo
