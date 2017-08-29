89ec96a978d0952d392f0ad2ce9e6b9d1df229cc
Samuel-Lewis <a1668737@student.adelaide.edu.au>
2017-08-29 22:11:20 +0930

    Switch to more HOF

--- app/controllers/clash_requests_controller.rb
+++ app/controllers/clash_requests_controller.rb
@@ -26,14 +26,12 @@ class ClashRequestsController < ApplicationController
         @clash_request = ClashRequest.find params[:id]
 
         # Load serialised data into nicer format
-        @old_student_sessions = []
-        @clash_request.preserve_student_sessions.each do |session|
-            @old_student_sessions << Session.find(session)
+        @old_student_sessions = @clash_request.preserve_student_sessions.map do |session|
+            Session.find(session)
         end
 
-        @old_clash_sessions = []
-        @clash_request.preserve_clash_sessions.each do |session|
-            @old_clash_sessions << Session.find(session)
+        @old_clash_sessions = @clash_request.preserve_clash_sessions.map do |session|
+            Session.find(session)
         end
     end
 
