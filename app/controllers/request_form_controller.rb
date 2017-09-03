6f5f878b96f4981962a111e185a638f04a43a976
a1641214 <a1641214@gmail.com>
2017-09-03 17:28:05 +0000

    #123 Add request type to new form

---
+++ app/controllers/request_form_controller.rb
@@ -0,0 +1,53 @@
+class RequestFormController < ApplicationController
+    layout "request_form"
+    def form_params
+        Time.zone = 'Adelaide'
+        time = Time.zone.now
+        params[:request_form] = params[:request_form].merge(:date_submitted => time.to_date)
+        params.require(:request_form).permit(
+            :enrolment_request_id,
+            :faculty,
+            :comments,
+            :student_id,
+            :date_submitted
+        )
+    end
+
+    def clash_resolution
+        session[:request_form] = RequestForm.get_session_data(session[:request_form])
+        @degrees = ["Software Engineering","Finance","Electronic Electricity","Telecommunication","Accounting"]
+        @semester = ["Summer semester, 2017","Semester2, 2017"]
+        @subject = ["COMP","MATH","C&ENVENG"]
+        @courses = Course.all
+    end
+
+    def unit_overload
+        session[:request_form] = RequestForm.get_session_data(session[:request_form])
+        session[:request_form] = RequestForm.get_session_data(session[:request_form])
+        @degrees = ["Software Engineering","Finance","Electronic Electricity","Telecommunication","Accounting"]
+        @semester = ["Summer semester, 2017","Semester2, 2017"]
+        @subject = ["COMP","MATH","C&ENVENG"]
+        @courses = Course.all
+    end
+
+    def course_full_resolution
+        session[:request_form] = RequestForm.get_session_data(session[:request_form])
+        @degrees = ["Software Engineering","Finance","Electronic Electricity","Telecommunication","Accounting"]
+        @semester = ["Summer semester, 2017","Semester2, 2017"]
+        @subject = ["COMP","MATH","C&ENVENG"]
+        @courses = Course.all
+    end
+
+    def create_clash_resolution
+        session[:request_form] = params[:request_form]
+        error = RequestForm.check_parameters(params)
+        if( error != nil )
+            flash[:form_error] = error
+            redirect_to "/clash_resolution"
+            return
+        end
+        @clash_request = RequestForm.create!(form_params)
+        flash[:notice] = "Clash request from student #{params[:student_id]} was created"
+        redirect_to clash_requests_path
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
     resources :clash_requests
-    resources :clash_resolution
     root to: 'clash_requests#index'
     get 'send_email', to: 'clash_requests#send_email', as: :send_email
     get 'confirmation' => 'clash_requests#confirmation', :as => :confirmation
     get 'demo' => 'demo#index', :as => :demo
+
+    # clash_form
+    get 'clash_resolution' => 'request_form#clash_resolution'
+    get 'unit_overload' => 'request_form#unit_overload'
+    get 'course_full_resolution' => 'request_form#course_full_resolution'
+    post 'clash_resolution' => 'request_form#create_clash_resolution'
+
     # Error pages
     match '/404', to: 'errors#not_found', via: :all
     match '/422', to: 'errors#unprocessable_entity', via: :all
