a7f87b3757f06a4444e2a11b1700273fd20fd87c
Markus James Andersons <markus.andersons@student.adelaide.edu.au>
2017-09-04 16:32:17 +0930

    #119 Update helper to show classes correctly on timetable

--- app/helpers/clash_requests_helper.rb
+++ app/helpers/clash_requests_helper.rb
@@ -1,10 +1,10 @@
 module ClashRequestsHelper
     def session_item(item)
-        displacement = [[(item[:session].time.hour - 9) * 100 + item[:session].time.min * (100 / 60), 0].max, 1000].min
-        height = [item[:session].length * 100, 0.5].max
+        displacement = [[((item[:session].time + 9.5.hours).hour - 8) * 100 + (item[:session].time + 9.5.hours).min * (100 / 60), 0].max, 1000].min
+        height = [item[:session].length/60 * 100, 0.5].max
         haml_tag :li, :class => ['session', ('requested' if item[:requested])], 'data-course' => item[:id], :style => "margin-top: #{displacement}px; height: #{height}px;" do
             haml_tag :a do
-                haml_tag :span, "#{item[:session].time.to_formatted_s(:time)} - #{(item[:session].time + item[:session].length.hours).to_formatted_s(:time)}", class: 'event-date'
+                haml_tag :span, "#{(item[:session].time + (9.5*60*60)).to_formatted_s(:time)} - #{(item[:session].time + (9.5*60*60) + item[:session].length.minutes).to_formatted_s(:time)}", class: 'event-date'
                 haml_tag :em, item[:session].component_code, class: 'event-name'
             end
         end
