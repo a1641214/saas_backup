a3a2d1c1b6979887b7478a47e354a94238b7d29b
Markus James Andersons <markus.andersons@student.adelaide.edu.au>
2017-09-10 13:07:41 +0930

    #119 Change colour of class numbers

--- app/helpers/clash_requests_helper.rb
+++ app/helpers/clash_requests_helper.rb
@@ -25,7 +25,7 @@ module ClashRequestsHelper
             haml_tag :a do
                 haml_tag :span, "#{(item[:session].time + (9.5 * 60 * 60)).to_formatted_s(:time)} - #{(item[:session].time + (9.5 * 60 * 60) + item[:session].length.minutes).to_formatted_s(:time)}", class: 'event-date'
                 haml_tag :em, item[:session].component_code, class: 'event-name'
-                haml_tag :span, class_number
+                haml_tag :span, class_number, class: 'class-number'
             end
         end
     end
